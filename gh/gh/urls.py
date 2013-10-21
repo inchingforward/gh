9d041f4300e630b6ea644560ef9ff359f01ffe5a
inchingforward <inchingforward@gmail.com>
2013-10-20 20:41:46 -0500

    Added preamble.

--- gh/gh/urls.py
+++ gh/gh/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
+
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -6,7 +8,8 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'gh.views.home', name='home'),
+    (r'^$', TemplateView.as_view(template_name="index.html")),
+    (r'^about/', TemplateView.as_view(template_name="about.html")),
     # url(r'^gh/', include('gh.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
