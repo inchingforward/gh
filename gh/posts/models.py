7c15521a1302797b12128f0b97d7b062e3ae73db
mj <inchingforward@gmail.com>
2013-10-24 12:29:42 -0500

    Added posts app and Post model.

---
+++ gh/posts/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    url = models.URLField(blank=True)
+    title = models.TextField()
+    details = models.TextField(blank=True)
+    visible = models.BooleanField(default=True)
+    allow_comments = models.BooleanField(default=True)
+    user = models.ForeignKey(User)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['-created']
+    
+    def __unicode__(self):
+        return self.title
