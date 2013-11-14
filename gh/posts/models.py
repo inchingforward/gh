28dba3fe4bf6f6e21e0e88c76aacfb0a1003c9c0
inchingforward <inchingforward@gmail.com>
2013-11-13 20:35:03 -0600

    Added Twitter integration.  Closes #17.

--- gh/posts/models.py
+++ gh/posts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
+import tweepy
 
 
 class Post(models.Model):
@@ -20,3 +22,17 @@ class Post(models.Model):
     
     def get_absolute_url(self):
         return "/posts/%s/" % self.id
+    
+    def save(self, *args, **kwargs):
+        super(Post, self).save(*args, **kwargs)
+        
+        if settings.TWITTER_CONSUMER_KEY:
+            try:
+                auth = tweepy.OAuthHandler(settings.TWITTER_CONSUMER_KEY, settings.TWITTER_CONSUMER_SECRET)
+                auth.set_access_token(settings.TWITTER_ACCESS_TOKEN, settings.TWITTER_ACCESS_TOKEN_SECRET)
+                
+                api = tweepy.API(auth)
+                api.update_status("%s %s" % (self.title[0:115], self.url))
+            except:
+                pass
+
