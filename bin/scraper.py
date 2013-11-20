e097ea4a00db240ea162049d89bf91bcfc33ceef
mj <inchingforward@gmail.com>
2013-11-20 13:26:55 -0600

    Added scraper placeholder.

---
+++ bin/scraper.py
@@ -0,0 +1,16 @@
+# Move the source urls into here and create scrapers for each 
+# url.  For each target link, print out the title of the link 
+# followed by the url.
+
+#http://www.bizjournals.com/stlouis/news
+#http://www.stltoday.com/search/?l=25&skin=/branding/stltoday/news&sd=desc&s=start_time&f=html&q=technology
+#https://www.stlbeacon.org/#!/list/AND[CATEGORY[innovation_stl]]
+#http://www.reddit.com/r/stlouis+programming
+#http://techli.com/news/
+#https://news.ycombinator.com/newest
+#http://monocle.io
+#http://www.techmeme.com/river
+#http://www.altdevblogaday.com
+
+if __name__ == '__main__':
+    print("Coming soon")
