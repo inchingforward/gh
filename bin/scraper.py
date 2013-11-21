d7f32780a4a2f1bc288f8b836cf5f741619f6b1a
mj <inchingforward@gmail.com>
2013-11-21 13:30:41 -0600

    Refactored.

--- bin/scraper.py
+++ bin/scraper.py
@@ -2,27 +2,21 @@ import requests
 from lxml import html
 
 
-def print_links(links, base_url=''):
-    for link in links:
-        print '%s:\n  %s%s\n' % (link.text, base_url, link.get('href')[1:])
-        
-def print_biz_journal_links():
-    print("\n\nSt. Louis Business Journal\n--------------------------")
-    res = requests.get('http://www.bizjournals.com/stlouis/news')
-    doc = html.fromstring(res.text)
-    links = doc.cssselect('li.object_subtype_blog_post h4 a')
-    print_links(links, doc.base)
-
-def print_reddit_links():
-    print("\n\nReddit Stl+Programming\n----------------------")
-    res = requests.get('http://www.reddit.com/r/stlouis+programming')
-    doc = html.fromstring(res.text)
-    links = doc.cssselect('div.thing div.entry a.title')
-    print_links(links)
+SITE_LINKS = (
+    ('St. Louis Business Journal', 'http://www.bizjournals.com/stlouis/news', 'li.object_subtype_blog_post h4 a', True),
+    ('Reddit', 'http://www.reddit.com/r/stlouis+programming', 'div.thing div.entry a.title', False),
+)
 
 if __name__ == '__main__':
-    print_biz_journal_links()
-    print_reddit_links()
+    for (name, url, selector, include_base) in SITE_LINKS:
+        print('\n\n%s\n%s' % (name, '-' * len(name)))
+        res = requests.get(url)
+        doc = html.fromstring(res.text)
+        links = doc.cssselect(selector)
+        base_url = doc.base[:-1] if include_base else ''
+        
+        for link in links:
+            print '%s:\n  %s%s\n' % (link.text, base_url, link.get('href'))
 
 # TODO:
 #http://www.stltoday.com/search/?l=25&skin=/branding/stltoday/news&sd=desc&s=start_time&f=html&q=technology
