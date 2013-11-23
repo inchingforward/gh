a214a2cea5f61f040a4f9a8da69dd35696653a62
inchingforward <inchingforward@gmail.com>
2013-11-22 21:27:33 -0600

    Added more sites.

--- bin/scraper.py
+++ bin/scraper.py
@@ -5,9 +5,12 @@ from lxml import html
 SITE_LINKS = (
     ('St. Louis Business Journal', 'http://www.bizjournals.com/stlouis/news', 'li.object_subtype_blog_post h4 a', True),
     ('Reddit', 'http://www.reddit.com/r/stlouis+programming', 'div.thing div.entry a.title', False),
+    ('Hacker News', 'http://news.ycombinator.com', 'td.title a', False),
+    ('Techli', 'http://techli.com/news', 'h2.post-title a', False),
+    ('TechMeme', 'http://www.techmeme.com', 'div.ii strong a', False),
 )
 
-if __name__ == '__main__':
+def print_site_links():
     for (name, url, selector, include_base) in SITE_LINKS:
         print('\n\n%s\n%s' % (name, '-' * len(name)))
         res = requests.get(url)
@@ -15,14 +18,8 @@ if __name__ == '__main__':
         links = doc.cssselect(selector)
         base_url = doc.base[:-1] if include_base else ''
         
-        for link in links:
+        for link in links[0:20]:
             print '%s:\n  %s%s\n' % (link.text, base_url, link.get('href'))
-
-# TODO:
-#http://www.stltoday.com/search/?l=25&skin=/branding/stltoday/news&sd=desc&s=start_time&f=html&q=technology
-#https://www.stlbeacon.org/#!/list/AND[CATEGORY[innovation_stl]]
-#http://techli.com/news/
-#https://news.ycombinator.com/newest
-#http://monocle.io
-#http://www.techmeme.com/river
-#http://www.altdevblogaday.com
+    
+if __name__ == '__main__':
+    print_site_links()
