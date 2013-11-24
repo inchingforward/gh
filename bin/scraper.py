f4024e27181a05ab152f06607c35211612638fb1
inchingforward <inchingforward@gmail.com>
2013-11-24 11:37:21 -0600

    Introduced transformers and fixed some site link urls.

--- bin/scraper.py
+++ bin/scraper.py
@@ -1,3 +1,21 @@
+"""
+scraper
+-------
+
+The scraper module scrapes pre-defined sites containing potential 
+Gateway Hackers news links.  The pre-defined sites in the 
+SITES list of maps contain the following keys:
+
+    name       : The name of the site, used for display purposes.
+    url        : The url of the site to scrape from.
+    selector   : The css selector that indentifies a list of links within
+                 the url result to scrape.
+    transformer: A lambda that takes the base url of the site 
+                 as well as the link to transform.  This is useful for sites that 
+                 use relative urls or modify a link's url in some way.
+"""
+
+import re
 import requests
 from lxml import html
 
@@ -6,42 +24,57 @@ SITES = [{
         'name': 'St. Louis Business Journal', 
         'url': 'http://www.bizjournals.com/stlouis/news', 
         'selector': 'li.object_subtype_blog_post h4 a', 
-        'include_base': True
+        'transformer': lambda base, url: base + url[1:],
     }, {
         'name': 'Reddit', 
         'url': 'http://www.reddit.com/r/stlouis+programming', 
         'selector': 'div.thing div.entry a.title', 
-        'include_base': False
+        'transformer': lambda base, url: re.sub(r'^/r/', 'http://reddit.com/r/', url)
     }, {
         'name': 'Hacker News', 
         'url': 'http://news.ycombinator.com', 
         'selector': 'td.title a', 
-        'include_base': False
+        'transformer': lambda base, url: re.sub(r'^item?id', base + '/item?id', url)
     }, {
         'name': 'Techli', 
         'url': 'http://techli.com/news', 
         'selector': 'h2.post-title a', 
-        'include_base': False
     }, {
         'name': 'TechMeme', 
         'url': 'http://www.techmeme.com', 
-        'selector': 'div.ii strong a', 
-        'include_base': False
+        'selector': 'div.ii strong a',
+        'transformer': lambda base, url: re.sub(r'^/goto/', 'http://', url)
     },
 ]
 
 def get_site_links():
+    """Iterates through SITES and creates a list of maps with the following
+    corresponding keys:
+    
+        name : The name of the site
+        links: A list of news links from the site
+    
+    Each link in the links list contains a 'text' key for the link's
+    text, and an 'href' which contains a fully-qualified url.
+    """
+    
     site_links = []
     
     for site in SITES:
         site_map = {'name': site['name'], 'links': []}
+        
         res = requests.get(site['url'])
         doc = html.fromstring(res.text)
         links = doc.cssselect(site['selector'])
-        base_url = doc.base[:-1] if site['include_base'] else ''
+        transformer = site.get('transformer', None)
+        base_url = doc.base or site['url']
         
         for link in links[0:20]:
-            href = base_url + link.get('href')
+            href = link.get('href')
+            
+            if transformer:
+                href = transformer(base_url, href)
+            
             site_map['links'].append({'text': link.text, 'href': href})
         
         site_links.append(site_map)
@@ -49,6 +82,8 @@ def get_site_links():
     return site_links
 
 def print_site_links(site_links):
+    """Prints a list of site_links maps."""
+    
     for site in site_links:
         print('\n\n%s\n%s' % (site['name'], '-' * len(site['name'])))
         for link in site['links']:
