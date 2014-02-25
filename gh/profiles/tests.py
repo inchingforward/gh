4853f0a9c3628e7877b63c0d5d50d8eff063a312
inchingforward <inchingforward@gmail.com>
2014-02-24 22:50:33 -0600

    Added profile tests.

--- gh/profiles/tests.py
+++ gh/profiles/tests.py
@@ -1,16 +1,56 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class ProfileTest(TestCase):
+    def test_uses_first_name_for_short_name(self):
+        user = User(username='testing', first_name='Atticus', last_name='Finch')
+        profile = Profile(user=user)
+        
+        self.assertEqual('Atticus', profile.get_short_name())
+    
+    def test_uses_username_when_missing_name(self):
+        user = User(username='testing')
+        profile = Profile(user=user)
+        
+        self.assertEqual('testing', profile.get_short_name())
+    
+    def test_full_name_no_name(self):
+        user = User(username='testing')
+        profile = Profile(user=user)
+        
+        self.assertEqual('testing', profile.get_full_name())
+    
+    def test_full_name_only_has_first_name(self):
+        user = User(username='testing', first_name='Atticus')
+        profile = Profile(user=user)
+        
+        self.assertEqual('Atticus', profile.get_full_name())
+    
+    def test_full_name_has_first_and_last(self):
+        user = User(username='testing', first_name='Atticus', last_name='Finch')
+        profile = Profile(user=user)
+        
+        self.assertEqual('Atticus Finch', profile.get_full_name())
+    
+    def test_has_projects_no_projects(self):
+        profile = Profile()
+        
+        self.assertFalse(profile.has_projects())
+    
+    def test_has_project_with_projects(self):
+        profile = Profile(project_1_url='http://gatewayhackers.com')
+        
+        self.assertTrue(profile.has_projects())
+    
+    def test_has_elsewhere_no_elsewheres(self):
+        profile = Profile()
+        
+        self.assertFalse(profile.has_elsewhere())
+    
+    def test_has_elswhere_with_elsewhere(self):
+        profile = Profile(elsewhere_1_url='http://gatewayhackers.com')
+        
+        self.assertTrue(profile.has_elsewhere())
+
