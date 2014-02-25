242c5ec9965e49474b76a317411aae1ec2c47bd4
inchingforward <inchingforward@gmail.com>
2014-02-24 22:28:50 -0600

    Added test for presence of posts.

--- gh/posts/tests.py
+++ gh/posts/tests.py
@@ -1,7 +1,9 @@
 from django.test import LiveServerTestCase
 from django.test import TestCase
+from django.contrib.auth.models import User
 from selenium import webdriver
 import views
+from .models import Post
 
 
 # Functional Tests
@@ -10,6 +12,7 @@ class HomePageTest(LiveServerTestCase):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
         self.browser.get(self.live_server_url)
+        self.user = User.objects.create(username='test')
     
     def tearDown(self):
         self.browser.quit()
@@ -25,3 +28,14 @@ class HomePageTest(LiveServerTestCase):
         self.assertGreaterEqual(len(self.browser.find_elements_by_link_text('Log in')), 1)
         self.assertGreaterEqual(len(self.browser.find_elements_by_link_text('Sign up')), 1)
         self.assertGreaterEqual(len(self.browser.find_elements_by_link_text('About')), 1)
+    
+    def test_home_page_with_posts(self):
+        for x in range(30):
+            item = Post()
+            item.title = 'Title %s' % x
+            item.visible = True
+            item.user = self.user
+            item.save()
+        
+        self.browser.get(self.live_server_url)
+        self.assertEqual(len(self.browser.find_elements_by_css_selector('#post-list li')), views.PAGINATE_BY)
