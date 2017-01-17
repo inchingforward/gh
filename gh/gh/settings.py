5a85aa78a38f0e014d273966df9668fe2528a6f3
Mike Janger <inchingforward@gmail.com>
2017-01-16 18:32:28 -0600

    Added new host.

--- gh/gh/settings.py
+++ gh/gh/settings.py
@@ -19,7 +19,7 @@ GOOGLE_ANALYTICS_ID = os.environ.get('GH_GOOGLE_ANALYTICS_ID', '')
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['.gatewayhackers.com']
+ALLOWED_HOSTS = ['.gatewayhackers.com', 'gh.mikejanger.net']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
